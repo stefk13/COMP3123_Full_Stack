b92ff5e85085c2aff9defc5c75d2c31474b7251b
Stefan <stefan.kepinski@georgebrown.ca>
2023-11-15 10:02:36 -0500

    Week 11 - Axios Ex added

---
+++ week11_axios_example/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/UserList'
+import UserList from './components/UserList';
+
+function App() {
+  return (
+    <div>
+      <h1>Week 11 - Axios and Navigation</h1>
+      <UserList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ week11_axios_example/src/components/UserDataRow.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+export default function UserDataRow(props) {
+  return (
+    <Fragment>
+        <div>
+            <img src={props.user.avatar} alt={props.user.first_name} />
+            <h2>{props.user.id}</h2>
+            <h3>{props.user.email}</h3>
+            <h3>{props.user.first_name} {props.user.last_name}</h3>
+        </div>
+    </Fragment>
+  )
+}
